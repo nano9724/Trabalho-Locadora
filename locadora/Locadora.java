92d8f21b007e34c70bdef59d26371a7c862a703b
nano9724 <51882529+nano9724@users.noreply.github.com>
2021-09-01 13:29:04 -0300

    Update Locadora.java

--- locadora/Locadora.java
+++ locadora/Locadora.java
@@ -1,7 +1,24 @@
+
 package locadora;
+
+import java.util.ArrayList;
+import pessoa.FileUtil;
+import pessoa.Usuario;
 
 
 public class Locadora {
-
-
+   
+   public static void main(String[] args){
+       String caminhoUsuarios="usuarios.txt";
+       ArrayList <Usuario>usuarios=new ArrayList<Usuario>();
+       
+       usuarios.add(new Usuario("Adriano","12345678910","nano","123456"));
+       usuarios.add(new Usuario("Victor", "12452548910","invict","985254"));
+       usuarios.add(new Usuario("Gabriel","12254826880","gabs","21547"));
+       usuarios.add(new Usuario("Vania",  "12254758274","van","984571"));
+       FileUtil.gravarObjeto(usuarios, caminhoUsuarios);
+   
+   }
+    
+    
 }
