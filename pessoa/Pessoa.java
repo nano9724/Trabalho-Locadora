63abd4f23e196b7253572da0da6389f278bf9497
nano9724 <51882529+nano9724@users.noreply.github.com>
2021-09-01 13:05:04 -0300

    Update Pessoa.java

--- pessoa/Pessoa.java
+++ pessoa/Pessoa.java
@@ -1,12 +1,13 @@
+
 package pessoa;
 
 public abstract class Pessoa{
 	private String nome;
-	private int cpf;
+	private String cpf;
 	private String login;
 	private String senha;
 
-	Pessoa(String nome, int cpf, String login, String senha){
+	Pessoa(String nome, String cpf, String login, String senha){
 	  this.nome=nome;
 	  this.cpf=cpf;
 	  this.login=login;
