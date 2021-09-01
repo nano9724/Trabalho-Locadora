6df7c64ab8bbc10790ccd7f0ba2a955ed4b4a31f
nano9724 <51882529+nano9724@users.noreply.github.com>
2021-09-01 01:08:43 -0300

    Update Usuario.java

--- pessoa/Usuario.java
+++ pessoa/Usuario.java
@@ -1,60 +1,96 @@
-package pessoa;
 
-import java.io.FileNotFoundException;
+package pessoa;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import locadora.Jogo;
 
 public class Usuario extends Pessoa implements Serializable{
-	private String email;
-	private int pontoFidelidade;
-	//private Map <Integer, Jogo> locacoes;
+       private String nome, login,senha, email;
+       private int pontoFidelidade,cpf;
+       private String caminho="alugueisUsuario.txt";
+       List <Jogo> locacoes=new ArrayList <Jogo>();
 
-	Usuario(String nome, int cpf, String login, String senha){
+	public Usuario(String nome, int cpf, String login, String senha){
 		super(nome,cpf,login,senha);
-		//this.locacoes=new HashMap<Integer, Jogo>();
-	}
-/*
-	public void logar(){
-		Scanner input = new Scanner(System.in);
-		System.out.println("Insira o seu login.");
-		String login = input.nextLine();
-		System.out.println("Insira a sua senha.");
-		String senha = input.nextLine();
+        }
+        
+        
+    void alugar(Jogo j){
+     this.locacoes.addAll((ArrayList <Jogo>)FileUtil.gravarObjeto(j, caminho));
+    }
+    
+    public Jogo procurarAluguel(Jogo j){
+    ArrayList<Jogo> obj= (ArrayList<Jogo>) FileUtil.recuperarObjeto(caminho);
+        
+    for(Jogo jogo: obj){
+                if (jogo.getId()==j.getId()){
+                   return jogo;}
+    }      return null;
+}
+    
+    void verAlugueis(){
+        ArrayList<Jogo> obj= (ArrayList<Jogo>) FileUtil.recuperarObjeto(caminho);
+        
+            for(Jogo jogo: obj){
+                jogo.toString();
+            }
+    }
 
-		if(login.equals("admin") && senha.equals("admin")) {
-			System.out.println("Olá " + login + " seja bem vindo ao Vapor!");
-		}
-		else{
-			System.out.println("Senha incorreta, por favor tente novamente.");
-			logar();
-		}
-	}
-	*/
-	
-/*
- * só cadastra um usuario pois sobrescreve o arquivo
-	public void cadastraUsuario() {
-		FileOutputStream fluxo;
-		
-		try {
-			fluxo= new FileOutputStream("usuar.ser");
-			ObjectOutputStream objarq;
-			objarq = new ObjectOutputStream(fluxo);
-			objarq.writeObject(this);
-			System.out.println("Salvo no arquivo");
-			objarq.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Passou por aqui");
-			e.printStackTrace();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	}
-*/
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPontoFidelidade(int pontoFidelidade) {
+        this.pontoFidelidade = pontoFidelidade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPontoFidelidade() {
+        return pontoFidelidade;
+    }
+        
+     @Override
+    public String toString(){
+    return String.format("%s\t%d", this.nome, this.cpf); //int %d\t; string %s\t
+    }   
+        
+        
 }
