8043c48c8487c10f79f407086170cb31dc9c22da
victormflorido <78482603+victormflorido@users.noreply.github.com>
2021-09-13 17:13:51 -0300

    Update Jogo.java

--- locadora/Jogo.java
+++ locadora/Jogo.java
@@ -5,11 +5,13 @@ import java.io.Serializable;
 public class Jogo implements Serializable{
 	  String nome, tipo;
 	  int classificao;
+	  double preco;
 
-	  Jogo(String nome, String tipo, int classificao){
+	  Jogo(String nome, String tipo, int classificao, double preco){
 	    this.nome=nome;
 	    this.tipo=tipo;
 	    this.classificao=classificao;
+	    this.preco=preco;
 	  }
 	  
 	  Jogo(){
