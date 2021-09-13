d7f7c59784971476a4c10eef46274e6343655122
Luis Adriano <51882529+nano9724@users.noreply.github.com>
2021-09-13 17:07:32 -0300

    Update Locadora.java

--- locadora/Locadora.java
+++ locadora/Locadora.java
@@ -15,7 +15,30 @@ public class Locadora {
        ArrayList <Usuario>usuarios=new ArrayList<Usuario>();
        ArrayList <Funcionario>funcionarios=new ArrayList<Funcionario>();
       
-      
+      System.out.println ("****Locadora****");
+       System.out.println ("****Digite o tipo de acesso****\n"+"1 - Usuário\n"+"2 - Funcionario");
+       Scanner teclado =new Scanner(System.in);
+      int opt=teclado.nextInt();
+      int option;
+      switch (opt){
+          
+          case 1: 
+           
+              System.out.println ("Digite:\n"+"1 - Login"+"2 - Cadastro Usuário");   
+              int user=teclado.nextInt();
+                        switch (user){ 
+                            case 1: 
+                                System.out.println("*** Digite o Login ***");
+                                Scanner teclado =new Scanner(System.in);
+                                String log=teclado.nextLine();
+                                System.out.println("*** Digite a Senha ***");
+                                String senha=teclado.nextLine();
+    
+                                    for(Usuario us: usuarios)
+                                            usuarios.clear();
+                                            usuarios.addAll((ArrayList<Usuario>)Arquivo.readObject());
+                                                for(i=0;i<usuarios.size();i++) {
+                                                    if(usuarios.logar())
        
        usuarios.add(new Usuario("Adriano","12345678910","nano","123456"));
        usuarios.add(new Usuario("Victor", "12452548910","invict","985254"));
