254d7ae3942c009dcc29e4c63bf951de4db3d773
victormflorido <78482603+victormflorido@users.noreply.github.com>
2021-09-14 22:04:45 -0300

    Update Usuario.java

--- pessoa/Usuario.java
+++ pessoa/Usuario.java
@@ -1,7 +1,12 @@
-
 package pessoa;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,141 +15,150 @@ import locadora.Locacoes;
 
 
 public class Usuario extends Pessoa implements Serializable{
-       private String nome, login,senha, email,cpf, caminhoUsuario, caminhoJogos;
-       private int pontoFidelidade;
-       private Conta conta;
-       List <Jogo> locacoes=new ArrayList <Jogo>();
+	private String  caminhoUsuario;
+	private double saldo;
+	static ArrayList <Usuario>usuarios=new ArrayList<Usuario>();
+	ArrayList<Jogo> Alugados= new ArrayList<Jogo>();
 
 	public Usuario(String nome, String cpf, String login, String senha){
 		super(nome,cpf,login,senha);
-                this.conta=new Conta();
-                this.caminhoUsuario=login.concat("User.ser");
-                File arquivo= new File(caminhoUsuario);
-                    if (!arquivo.exists()){
-                        try{
-                        arquivo.createNewFile();
-                         }catch(Exception e)
-                            {e.printStackTrace();}
-                        
-                this.caminhoJogos=login.concat("Jogos.ser");
-                File arquivoJogo= new File(caminhoJogos);
-                    if (!arquivo.exists()){
-                        try{
-                        arquivo.createNewFile();
-                         }catch(Exception e)
-                            {e.printStackTrace();}        
-        }}}
-        
-        
-        
-        
-    public void alugar(){
-        Locacoes.locar(this);
-        this.locacoes.addAll((ArrayList<Jogo>) Arquivo.recuperarObjeto(caminhoJogos));
-        this.pontoFidelidade+=5;
-        
-     }
-    
-    public Jogo procurarAluguel(Jogo j){
-    ArrayList<Jogo> obj= (ArrayList<Jogo>) Arquivo.recuperarObjeto(caminhoJogos);
-        
-    for(Jogo jogo: obj){
-                if (jogo.getId()==j.getId()){
-                   return jogo;}
-    }      return null;
-}
-    
-   public void verAlugueis(){
-        ArrayList<Jogo> obj= (ArrayList<Jogo>) Arquivo.recuperarObjeto(caminhoJogos);
-        
-            for(Jogo jogo: obj){
-                jogo.toString();
-            }
-    }    
-   
-  public void pesquisarJogos(){
-       Locacoes.leDisp();
-   }
-   
-   public void devolver(){
-        Locacoes.devolucao(this);
-        this.locacoes.addAll((ArrayList<Jogo>) Arquivo.recuperarObjeto(caminhoJogos));
-        
-   }
-        
-     @Override
-    public String toString(){
-    return String.format("%s\t%s", this.nome, this.cpf); //int %d\t; string %s\t
-    }   
-        
-    public boolean logar(String login, String senha){
-	         if(login.equals(this.getLogin()) && senha.equals(this.getSenha())) {
-	            System.out.println("Olá " + this.getNome() + " seja bem vindo ao Vapor!");
-		    return true;
-	        }
-	        else{
-	            System.out.println("Senha incorreta, por favor tente novamente.");
-	            return false;
-	        }
-                 
-	    }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+		this.caminhoUsuario=login.concat("User.ser");
+		FileOutputStream fluxo;
+		try {
+			fluxo= new FileOutputStream(caminhoUsuario);
+			ObjectOutputStream objarq;
+			objarq = new ObjectOutputStream(fluxo);
+			Alugados.clear();
+			objarq.writeObject(Alugados);
+			objarq.close();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+	public Usuario() {
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	}
 
-    public String getNome() {
-        return nome;
-    }
 
-    public String getLogin() {
-        return login;
-    }
+	public static void criaArquivoUsuario( ) {
+		FileOutputStream fluxo;
+		try {
+			fluxo= new FileOutputStream("usuarios.ser");
+			ObjectOutputStream objarq;
+			objarq = new ObjectOutputStream(fluxo);
+			usuarios.clear();
+			objarq.writeObject(usuarios);
+			objarq.close();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    public String getSenha() {
-        return senha;
-    }
+	}
+	/*
+	public static void leUsur() {
+		FileInputStream fluxo;
+		try {
+			int i;
+			fluxo= new FileInputStream("usuarios.ser");
+			ObjectInputStream objarq;
+			objarq = new ObjectInputStream(fluxo);
+			System.out.println("oi");
+			usuarios=(ArrayList<Usuario>)objarq.readObject();
+			System.out.println("oi2");
+			for(i=0;i<usuarios.size();i++) {
+				System.out.println(i+": "+usuarios.get(i).getLogin()+ " "+usuarios.get(i).getSenha());
+			}
+			objarq.close();
 
-    public String getCaminhoUsuario() {
-        return caminhoUsuario;
-    }
 
-    public String getCaminhoJogos() {
-        return caminhoJogos;
-    }
+		} catch (FileNotFoundException e) {
 
-    public String getCpf() {
-        return cpf;
-    }
+			//e.printStackTrace();
+			System.out.println("Passou por aqui");
+		}
+		catch (IOException e) {
+			//e.printStackTrace();
+			System.out.println("Passou por aqui2");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Passou por aqui3");
+			//e.printStackTrace();
+		}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
-    public void setPontoFidelidade(int pontoFidelidade) {
-        this.pontoFidelidade = pontoFidelidade;
-    }
+	}
 
-    public String getEmail() {
-        return email;
-    }
+*/
+  
 
-    public int getPontoFidelidade() {
-        return pontoFidelidade;
-    }
-    
-    
-        
+	public boolean logar(String login, String senha){
+		if(login.equals(this.getLogin()) && senha.equals(this.getSenha())) {
+			System.out.println("Olá " + this.getNome() + " seja bem vindo ao Vapor!");
+			return true;
+		}
+		else{
+			return false;
+		}
+
+	}
+
+
+
+	public String getCaminhoUsuario() {
+		return caminhoUsuario;
+	}
+
+
+
+
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double valor) {
+		FileInputStream fluxo;
+		FileOutputStream fluxo2;
+		try {
+			int i;
+			fluxo= new FileInputStream("usuarios.ser");
+			ObjectInputStream objarq;
+			objarq = new ObjectInputStream(fluxo);
+			usuarios=(ArrayList<Usuario>)objarq.readObject();
+			for(i=0;i<usuarios.size();i++) {
+				//System.out.println(usuarios.get(i).getLogin());
+				if(usuarios.get(i).getLogin().equals(this.getLogin())==true) {
+					usuarios.get(i).saldo=valor;
+					this.saldo=valor;
+					
+				}
+			}
+			fluxo2= new FileOutputStream("usuarios.ser");
+			ObjectOutputStream objarq2;
+			objarq2 = new ObjectOutputStream(fluxo2);
+			objarq2.writeObject(usuarios);
+			objarq.close();
+			objarq2.close();
+
+		} catch (FileNotFoundException e) {
+
+			//e.printStackTrace();
+			System.out.println("Passou por aqui");
+		}
+		catch (IOException e) {
+			//e.printStackTrace();
+			System.out.println("Passou por aqui2");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Passou por aqui3");
+			//e.printStackTrace();
+		}
+	}
+
 }
