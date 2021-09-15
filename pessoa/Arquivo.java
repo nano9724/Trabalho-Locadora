065d4cc06516499b8e19a05569c1618ff416f6f4
victormflorido <78482603+victormflorido@users.noreply.github.com>
2021-09-15 17:46:35 -0300

    Update Arquivo.java

--- pessoa/Arquivo.java
+++ pessoa/Arquivo.java
@@ -10,54 +10,57 @@ import java.io.ObjectOutputStream;
 
 
 public class Arquivo {
-    
-    public static Object gravarObjeto(Object obj, String caminho){
-        File arquivo= new File(caminho);
-        if (!arquivo.exists()){
-        try{
-            arquivo.createNewFile();
-        }catch(Exception e)
-        {e.printStackTrace(); 
-        return false;}
-        }
-        try{
-            FileOutputStream fileOutput= new FileOutputStream(arquivo);
-            ObjectOutputStream objOutput= new ObjectOutputStream(fileOutput);
-            
-            objOutput.writeObject(obj);
-            
-            objOutput.flush();
-            fileOutput.flush();
-            objOutput.close();
-            fileOutput.close();
-            
-            return obj;}
-        
-        catch(Exception e)
-            {e.printStackTrace(); return null;}
-    }
-    
-    public static Object recuperarObjeto(String caminho){
-        File arquivo= new File(caminho);
-        
-        try{
-            
-            FileInputStream fileInput= new FileInputStream(arquivo);
-            ObjectInputStream objectInput= new ObjectInputStream(fileInput);
-            
-            Object retorno=objectInput.readObject();
 
-            objectInput.close();
-            
-            return retorno;}
-        
-        catch(Exception e){
-        	//e.printStackTrace();
-        	return null;
-        	} 
-        }
-    
-    
-    
-    
+	public static Object gravarObjeto(Object obj, String caminho){
+		File arquivo= new File(caminho);
+		if (!arquivo.exists()){
+			try{
+				arquivo.createNewFile();
+			}catch(Exception e)
+			{ 
+			return false;}
+		}
+		try{
+			FileOutputStream fileOutput= new FileOutputStream(arquivo);
+			ObjectOutputStream objOutput= new ObjectOutputStream(fileOutput);
+
+			objOutput.writeObject(obj);
+
+			objOutput.flush();
+			fileOutput.flush();
+			objOutput.close();
+			fileOutput.close();
+
+			return obj;}
+
+		catch(Exception e){
+			
+			return null;
+		}
+	}
+
+	public static Object recuperarObjeto(String caminho){
+		File arquivo= new File(caminho);
+
+		try{
+
+			FileInputStream fileInput= new FileInputStream(arquivo);
+			ObjectInputStream objectInput= new ObjectInputStream(fileInput);
+
+			Object retorno=objectInput.readObject();
+
+			objectInput.close();
+
+			return retorno;}
+
+		catch(Exception e){
+			System.out.println("Arquivo inexistente...\nCriando arquivo!");
+			Usuario.criaArquivoUsuario();
+			return recuperarObjeto(caminho);
+		} 
+	}
+
+
+
+
 }
