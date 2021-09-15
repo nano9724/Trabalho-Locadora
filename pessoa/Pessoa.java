a07f228b5d1007e9cc0e0ee93b77e25888bf5a90
victormflorido <78482603+victormflorido@users.noreply.github.com>
2021-09-14 22:05:11 -0300

    Update Pessoa.java

--- pessoa/Pessoa.java
+++ pessoa/Pessoa.java
@@ -1,7 +1,8 @@
-
 package pessoa;
 
-public abstract class Pessoa{
+import java.io.Serializable;
+
+public abstract class Pessoa implements Serializable{
 	private String nome;
 	private String cpf;
 	private String login;
@@ -14,5 +15,37 @@ public abstract class Pessoa{
 	  this.senha=senha;
 	}
 
+	Pessoa(){
+		
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+	public String getCpf() {
+		return cpf;
+	}
 }
