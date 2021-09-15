e77550b994aa5ba08d11e6c6948f9d8ce20e5589
victormflorido <78482603+victormflorido@users.noreply.github.com>
2021-09-14 22:03:29 -0300

    Update Locadora.java

--- locadora/Locadora.java
+++ locadora/Locadora.java
@@ -1,5 +1,11 @@
 package locadora;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 import pessoa.Arquivo;
@@ -9,29 +15,47 @@ import pessoa.Usuario;
 public class Locadora {
 
 	public static void UserIntialPage(Usuario u){
+
 		Scanner teclado=new Scanner(System.in);
-		System.out.println ("Digite:\n"+"1 - Alugar\n"+"2 - Ver seus Alugueis\n"+"3 - Devolução\n"+"4 - Ver Jogos Disponiveis na Locadora\n");   
+		System.out.println ("\nDigite:\n"+"(1) - Alugar\n"+"(2) - Ver seus Alugueis\n"+"(3) - Devolução\n"+"(4) - Ver Jogos Disponiveis na Locadora\n"+"(5) - Adicionar fundos\n(6) - Ver Fundos\n(7) - Para sair");   
 		int user=teclado.nextInt();
-		switch (user){ 
-		case 1:
-			u.alugar();
+		while(user!=7) {
+			switch (user){ 
+			case 1:
+				Locacoes.locar(u);
+				u.setSaldo(u.getSaldo());
+				break;
 
-		case 2: 
-			u.verAlugueis();
+			case 2: 
+				Locacoes.leAlug(u);
+				break;
 
-		case 3:
-			u.devolver();
+			case 3:
+				Locacoes.devolucao(u);
+				break;
 
-		case 4: 
-			u.pesquisarJogos();
-		default:
-
-		}}
+			case 4: 
+				Locacoes.leDisp();
+				break;
+			case 5:
+				System.out.println("Quanto deseja adicionar?");
+				int val=teclado.nextInt();
+				u.setSaldo(u.getSaldo()+val);
+				break;
+			case 6:
+				System.out.println(u.getSaldo());
+				break;
+			default:
+				System.out.println("Digite um valor valido");
+			}
+			System.out.println ("\nDigite:\n"+"(1) - Alugar\n"+"(2) - Ver seus Alugueis\n"+"(3) - Devolução\n"+"(4) - Ver Jogos Disponiveis na Locadora\n"+"(5) - Adicionar fundos\n(6) - Ver Fundos\n(7) - Para sair");     
+			user=teclado.nextInt();
+		}
+	}
 
 
 
 	public static void main(String[] args){
-		String caminhoUsuarios="usuarios.txt";// Estrutura para manipular todos os usuários
 		ArrayList <Usuario>usuarios=new ArrayList<Usuario>();
 
 		String caminhoFuncionarios="funcionarios.txt";// Estrutura para manipular todos os funcionarios
@@ -48,25 +72,31 @@ public class Locadora {
 		switch (opt){
 
 		case 1: //Usuário
-
-			System.out.println ("Digite:\n"+"1 - Login"+"2 - Cadastro Usuário");   
+			System.out.println ("Digite:\n"+"1 - Login\n"+"2 - Cadastro Usuário");   
 			int user=teclado.nextInt();
 			switch (user){ 
 			case 1: 
-				System.out.println("*** Digite o Login ***");
-				teclado =new Scanner(System.in);
-				String log=teclado.nextLine();
-				System.out.println("*** Digite a Senha ***");
-				String senha=teclado.nextLine();
+				boolean aju=false;
+				while(aju==false) {
+					System.out.println("*** Digite o Login ***");
+					teclado =new Scanner(System.in);
+					String log=teclado.nextLine();
+					System.out.println("*** Digite a Senha ***");
+					String senha=teclado.nextLine();
 
-				for(Usuario us: usuarios)
 					usuarios.clear();
-				usuarios.addAll((ArrayList<Usuario>)Arquivo.recuperarObjeto(caminhoUsuarios));
-				for(int i=0;i<usuarios.size();i++) {
-					if(usuarios.get(i).logar(log, senha)){
-						UserIntialPage(usuarios.get(i));
-					}}
-
+					usuarios.addAll((ArrayList<Usuario>)Arquivo.recuperarObjeto("usuarios.ser"));
+					for(int i=0;i<usuarios.size();i++) {
+						aju=usuarios.get(i).logar(log, senha);
+						if(aju==true){
+							UserIntialPage(usuarios.get(i));
+							break;
+						}}
+					if(aju==false) {
+						System.out.println("Login ou Senha incorreto");
+					}
+				}
+				break;
 			case 2: 
 				System.out.println("*** Cadastro ***");
 				System.out.println("*** Crie um Login ***");
@@ -79,19 +109,44 @@ public class Locadora {
 				System.out.println("*** Por fim informe o seu cpf ***");
 				String cpf=teclado.nextLine();
 				Usuario usuario= new Usuario(nome, cpf, login, psw);
-				usuarios.clear();
-				Arquivo.gravarObjeto(usuario,caminhoUsuarios);
-				usuarios.addAll((ArrayList<Usuario>)Arquivo.recuperarObjeto(caminhoUsuarios));
-				System.out.println("passou");
-				usuarios.add(usuario);
-				Arquivo.gravarObjeto(usuarios,caminhoUsuarios);
+				FileInputStream fluxo;
+				FileOutputStream fluxo2;
+				try {
+					fluxo= new FileInputStream("usuarios.ser");
+					ObjectInputStream objarq;
+					objarq = new ObjectInputStream(fluxo);
+					usuarios.clear();
+					usuarios.addAll((ArrayList<Usuario>)objarq.readObject());
+					usuarios.add(usuario);
+					fluxo2= new FileOutputStream("usuarios.ser");
+					ObjectOutputStream objarq2;
+					objarq2 = new ObjectOutputStream(fluxo2);
+					objarq2.writeObject(usuarios);
+					System.out.println("Salvo no arquivo");
+					System.out.println("O usuario "+ login+ " foi criado");
+					objarq.close();
+					objarq2.close();
 
+				} catch (FileNotFoundException e) {
+					System.out.println("Arquivo nao encontrado");
+					Usuario.criaArquivoUsuario();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+				catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
+				break;
 
 
 
 			}
+			break;
 		case 2:
+
 			String filepath = "account.txt";
 			System.out.println("**** Digite o login ****");
 			String username = teclado.next();
@@ -105,7 +160,7 @@ public class Locadora {
 			}
 			System.out.println("**** Bem vindo ao menu de Funcionário ****");
 			System.out.println(
-					"Você deseja:\n(1) - Criar uma nova conta de funcionário\n(2) - Adicionar jogo\n(3) - Listar jogos disponiveis");
+					"Você deseja:\n(1) - Criar uma nova conta de funcionário\n(2) - Adicionar jogo\n(3) - Listar jogos disponiveis\n(4) - Para sair");
 			int funcMenu = teclado.nextInt();
 			while (funcMenu != 4) {
 				switch (funcMenu) {
@@ -127,9 +182,13 @@ public class Locadora {
 					break;
 				case 3:
 					Locacoes.leDisp();
+					break;
+				default:
+					System.out.println("Numero invalido");	
 				}
+
 				System.out.println(
-						"Você deseja:\n(1) - Criar uma nova conta de funcionário\n(2) - Adicionar jogo\n(3) - Listar jogos disponiveis");
+						"Você deseja:\n(1) - Criar uma nova conta de funcionário\n(2) - Adicionar jogo\n(3) - Listar jogos disponiveis\n(4) - Para sair");
 				funcMenu = teclado.nextInt();
 			}
 
